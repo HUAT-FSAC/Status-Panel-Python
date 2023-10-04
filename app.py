c6abf37ddbe8a092ea7c990d84d5b188271e0018
NekoRectifier <hyx123250@gmail.com>
2023-10-04 14:20:30 +0800

    feat: basic structure

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
+# global
 app = Flask(__name__)
-socketio = SocketIO(app)
-# TODO: using socket.io to update values in realtime
+# socketio = SocketIO(app)
 
+# web route
 @app.route('/')
 def index():
     return render_template("index.html", current_speed=0.0, current_lat=0.0, current_lon=0.0)
 
-@app.route('/settings')
-def settings():
-    return "das"
+@app.route('/update')
+def update():
+    return '{"speed": 100}';
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    socketio.run(app)
+    app.run()
