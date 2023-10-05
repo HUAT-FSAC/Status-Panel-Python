f1010e92bc7f85907c5ea32f825d2e3c3a3d7bbb
NekoRectifier <hyx123250@gmail.com>
2023-10-05 11:18:31 +0000

    fix: VD varibles init & add alt factor & minor improve

--- scripts/main.py
+++ scripts/main.py
@@ -1,26 +1,21 @@
-#!/usr/bin/python
-# coding:utf-8
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 
-import rospy
-import web
-import math
-import json
+import rospy, web, math, json, time, pprint
 from common_msgs.msg import HUAT_ASENSING, vehicle_status
 
 class VehicleData():
-    json_data = "{}"
-    imu_v_n = 0
-    imu_v_e = 0
-    imu_v_g = 0
-    imu_v = 0
-    imu_lat = 0
-    imu_lon = 0
-    vcu_steering = 0;
-    vcu_throttle = 0;
-    vcu_wheel_spd = 0;
-
     def __init__(self):
-        pass
+        self.imu_v_n = 0
+        self.imu_v_e = 0
+        self.imu_v_g = 0
+        self.imu_v = 0
+        self.imu_lat = 0
+        self.imu_lon = 0
+        self.imu_alt = 0
+        self.vcu_steering = 0
+        self.vcu_throttle = 0
+        self.vcu_wheel_spd = 0
 
     def vcu_upload(self, data):
         self.vcu_steering = data.steering
@@ -34,11 +29,13 @@ class VehicleData():
 
         self.imu_v = math.sqrt(math.pow(self.imu_v_n, 2) + math.pow(self.imu_v_e, 2) + math.pow(self.imu_v_g, 2))
         self.imu_lat = data.latitude
-        self.imu_lon= data.longitute
+        self.imu_lon= data.longitude
+        self.imu_alt = data.altitude
         self.download()
 
     def download(self):
-        self.json_data = json.dumps(self.__dict__)
+        pprint.pprint(json.dumps(self.__dict__))
+        web.update(json.dumps(self.__dict__))
 
 def imu_callback(data):
     vehicleData.imu_upload(data)
@@ -50,10 +47,9 @@ if __name__ == "__main__":
     vehicleData = VehicleData()
     rospy.init_node("status_panel")
     rospy.Subscriber("/INS/ASENSING_INS", HUAT_ASENSING, imu_callback)
-    rospy.Subscriber("/vehicleStatus", vehicle_status, vcu_callback)
-    
-    web.run()
-    # TODO: better ways to stop
+    # rospy.Subscriber("/vehicleStatus", vehicle_status, vcu_callback)
+    # not sure the real topic name
+
     rospy.spin()
 
 
