c4d184509743d376187c81f4e372d9bb26bb3049
NekoRectifier <hyx123250@gmail.com>
2023-10-05 08:56:17 +0000

    fix: update js ver of socketio to fix incompatible

--- scripts/web.py
+++ scripts/web.py
@@ -1,28 +1,42 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: UTF-8 -*-
+# this code must run in python3, otherwise socketio will not work
 
-import threading
-import main
 from flask import Flask, render_template
-# from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO
 
 # global
-app = Flask(__name__)
-# socketio = SocketIO(app)
+app = Flask(__name__, template_folder='./template')
+socketio = SocketIO(app)
 
 # web route
 @app.route('/')
 def index():
     return render_template("index.html", current_speed=0.0, current_lat=0.0, current_lon=0.0)
 
-@app.route('/update')
-def update():
-    return main.VehicleData.json_data
+@socketio.on('connect')
+def handle_connection():
+    print('frontend has connected')
+    socketio.emit("connection", "backend connected")
 
-def run():
-    t = threading.Thread(target=app.run(host="0.0.0.0"))
-    t.daemon = True
-    t.start()
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('frontend disconnected')
+    socketio.emit("connection", "backend disconnected")
+
+@socketio.on('msg')
+def msg(data):
+    # actually msg callback
+    print("msg" + data)
+    pass
+
+@socketio.on("control")
+def control(cmd):
+    #TODO
+    pass
+
+def update(data):
+    socketio.emit("server_response", data)
 
 if __name__ == "__main__":
-    print("what?")
+    socketio.run(app, host='0.0.0.0', port=5000)
